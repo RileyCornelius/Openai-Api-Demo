fbfd71525c1d53b63012326f6ba8a80bc84b2e7c
Riley Cornelius <54592152+RileyCornelius@users.noreply.github.com>
2023-11-25 16:52:41 -0600

    Remove tests

--- tests/huggingface_test.py
+++
@@ -1,9 +0,0 @@
-import gradio as gr
-
-from transformers import pipeline
-
-# https://huggingface.co/models?pipeline_tag=text-generation&sort=trending
-pipe = pipeline(model="mistralai/Mistral-7B-v0.1")
-
-demo = gr.Interface.from_pipeline(pipe)
-demo.launch()
