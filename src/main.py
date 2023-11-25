b69624d2c9c972dab68955741801f9291cbb1ce4
Riley Cornelius <54592152+RileyCornelius@users.noreply.github.com>
2023-11-25 16:41:26 -0600

    Refactor openai_client.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from ui import create_ui
-from openai_client import set_openai_key
 
 
 if __name__ == "__main__":
-    set_openai_key()
     create_ui()
--- src/openai_client.py
+++ src/openai_client.py
@@ -6,60 +6,19 @@ from openai import OpenAI
 
 from utils import *
 
-
+load_dotenv()
 openai_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_key)
 
 
-def set_openai_key():
-    if load_dotenv():
-        openai_key = os.getenv("OPENAI_API_KEY")
-        client.api_key = openai_key
-
-
-def chat(
-    prompt,
-    model: Literal[
-        "gpt-4",
-        "gpt-4-1106-preview",
-        "gpt-3.5-turbo",
-        "gpt-3.5-turbo-16k",
-    ] = "gpt-3.5-turbo",
-) -> str:
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            },
-        ],
-    )
-
+def chat(prompt, model: Literal["gpt-4", "gpt-4-1106-preview", "gpt-3.5-turbo", "gpt-3.5-turbo-16k"] = "gpt-3.5-turbo") -> str:
+    completion = client.chat.completions.create(model=model, messages=[{"role": "user", "content": prompt}])
     response = completion.choices[0].message.content
     return response
 
 
-def chat_stream(
-    prompt,
-    model: Literal[
-        "gpt-4",
-        "gpt-4-1106-preview",
-        "gpt-3.5-turbo",
-        "gpt-3.5-turbo-16k",
-    ] = "gpt-3.5-turbo",
-):
-    stream = client.chat.completions.create(
-        stream=True,
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-    )
-
+def chat_stream(prompt, model: Literal["gpt-4", "gpt-4-1106-preview", "gpt-3.5-turbo", "gpt-3.5-turbo-16k"] = "gpt-3.5-turbo"):
+    stream = client.chat.completions.create(stream=True, model=model, messages=[{"role": "user", "content": prompt}])
     return stream
 
 
@@ -69,28 +28,6 @@ def print_chat_stream(stream):
         print(chunk or "", end="", flush=True)
 
 
-def generate_image(
-    prompt: str,
-    model: Literal["dall-e-2", "dall-e-3"],
-    quality: Literal["standard", "hd"],
-    size: Literal["1024x1024", "1024x1792", "1792x1024"],
-) -> str:
-    try:
-        response = client.images.generate(
-            prompt=prompt,
-            model=model,
-            quality=quality,
-            size=size,
-            n=1,
-        )
-    except Exception as error:
-        print("Dall-e error: " + error.code)
-        return ""
-
-    url = response.data[0].url
-    return url
-
-
 def speech_to_text(
     audio,
     model: Literal["whisper-1"] = "whisper-1",
@@ -98,32 +35,40 @@ def speech_to_text(
 ) -> str:
     try:
         audio_file = open(audio, "rb")
-        transcriptions = client.audio.transcriptions.create(model=model, file=audio_file, response_format=response_type)
+        transcription = client.audio.transcriptions.create(model=model, file=audio_file, response_format=response_type)
+        return transcription
     except Exception as error:
-        print(print("Speech to text error: " + error.code))
+        print(f"Speech to text error: {error.code}")
         return ""
-
-    return transcriptions
 
 
 def text_to_speech(
     text: str,
-    model: Literal["tts-1", "tts-1-hd"],
+    model: Literal["tts-1", "tts-1-hd"] = "tts-1-hd",
     voice: Literal["alloy", "echo", "fable", "onyx", "nova", "shimmer"] = "alloy",
     output_file_format: Literal["mp3", "opus", "aac", "flac", ""] = "mp3",
     speed: float = 1.0,
 ) -> str:
     try:
-        response = client.audio.speech.create(
-            input=text,
-            model=model,
-            voice=voice,
-            response_format=output_file_format,
-            speed=speed,
-        )
+        response = client.audio.speech.create(input=text, model=model, voice=voice, response_format=output_file_format, speed=speed)
+        audio_path = cache_audio(response.content)
+        return audio_path
     except Exception as error:
-        print("Text to speech error: " + error.code)
+        print(f"Text to speech error: {error.code}")
         return ""
 
-    audio_path = cache_audio(response.content)
-    return audio_path
+
+def generate_image(
+    prompt: str,
+    model: Literal["dall-e-2", "dall-e-3"] = "dall-e-3",
+    quality: Literal["standard", "hd"] = "hd",
+    size: Literal["1024x1024", "1024x1792", "1792x1024"] = "1024x1024",
+    style: Literal["vivid", "natural"] = "vivid",
+) -> str:
+    try:
+        response = client.images.generate(prompt=prompt, model=model, quality=quality, size=size, style=style, n=1)
+        url = response.data[0].url
+        return url
+    except Exception as error:
+        print(f"Dall-e error:  {error.code}")
+        return ""
